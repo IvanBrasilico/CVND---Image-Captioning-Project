4924fb74d1959c30e8c6c78124fa9bebd15df648
IvanBrasilico <Iv@n1234>
2020-02-27 15:53:42 -0300

    Modifications

--- model.py
+++ model.py
@@ -23,11 +23,38 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, 
+                            num_layers=num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.hidden = None
+    
     
     def forward(self, features, captions):
-        pass
-
+        """Decode image feature vectors and generates captions."""
+        captions = captions[:,:-1]
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        output, _ = self.lstm(embeddings)
+        outputs = self.linear(output)
+        return outputs
+    
+    
     def sample(self, inputs, states=None, max_len=20):
-        " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        """ Accepts pre-processed image tensor (inputs) and
+        returns predicted sentence (list of tensor ids of length max_len)"""
+        sampled_ids = []
+        batch_size = inputs.shape[0]
+        print("Batch_size  " , batch_size)
+        for i in range(max_len):
+            lstm_states, _ = self.lstm(inputs) 
+            outputs = self.linear(lstm_states.squeeze(1)) 
+            predicted = outputs.max(1)[1]
+            sampled_ids.append(predicted.cpu().numpy()[0].item())
+            if predicted == 1:
+                break
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)
+        return sampled_ids
+    
